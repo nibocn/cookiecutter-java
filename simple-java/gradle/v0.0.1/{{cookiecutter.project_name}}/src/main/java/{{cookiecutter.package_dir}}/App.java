6d9b94d66aa6ada283cc7c77f1e5321f892fb7a6
NiBo <nibocn@gmail.com>
2018-07-31 15:52:37 +0800

    feat: Add lombok support

--- simple-java/gradle/v0.0.1/{{cookiecutter.project_name}}/src/main/java/{{cookiecutter.package_dir}}/App.java
+++ simple-java/gradle/v0.0.1/{{cookiecutter.project_name}}/src/main/java/{{cookiecutter.package_dir}}/App.java
@@ -1,21 +1,34 @@
 package {{ cookiecutter.package }};
-
+{% if cookiecutter.enabled_lombok == 'yes' %}
+import lombok.extern.slf4j.Slf4j;
+{%- else %}
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+{%- endif %}
 
 /**
  * Hello World
  *
  * @author {{ cookiecutter.full_name }}
  */
+{%- if cookiecutter.enabled_lombok == 'yes' %}
+@Slf4j
+{%- endif %}
 @SuppressWarnings({"PMD.UseUtilityClass", "PMD.ClassNamingConventions"})
 public class App {
+    {%- if cookiecutter.enabled_lombok == 'no' %}
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    {%- endif %}
 
     // CSOFF: Javadoc
     @SuppressWarnings("PMD.UnusedFormalParameter")
     public static void main(String[] args) {
+        {%- if cookiecutter.enabled_lombok == 'yes' %}
+        log.debug("Hello World!");
+        {%- else %}
         LOGGER.debug("Hello World!");
+        {%- endif %}
+
     }
     // CSON: Javadoc
 }
